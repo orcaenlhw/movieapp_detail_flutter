1d0fa374722f23207b18223851b1e60bf425aad9
edn <45616806+theednorc@users.noreply.github.com>
2022-03-13 01:18:48 +0630

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/movieapp_detail_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.movieapp_detail_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:movieapp_detail_flutter/pages/main_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.grey, scaffoldBackgroundColor: Colors.grey),
+      home: MainPage(),
+    );
+  }
+}
